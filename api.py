be4d4a29ae25f148dbb4ed2d18f9d62e493e1b93
Claes Nilsson <claes.nilsson13@gmail.com>
2025-05-30 07:54:47 +0200

    first working one

--- api.py
+++ api.py
@@ -1,23 +1,25 @@
 import requests
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class ProxmoxBackupAPI:
-    def __init__(self, host, username, password):
+    def __init__(self, host, token_id, token_secret):
         self.base_url = f"https://{host}:8007/api2/json"
         self.session = requests.Session()
-        self._login(username, password)
-
-    def _login(self, username, password):
-        url = f"{self.base_url}/access/ticket"
-        data = {"username": username, "password": password}
-        resp = self.session.post(url, data=data, verify=False)
-        resp.raise_for_status()
-        ticket = resp.json()["data"]["ticket"]
-        csrf = resp.json()["data"]["CSRFPreventionToken"]
-        self.session.cookies.set("PBSAuthCookie", ticket)
-        self.session.headers.update({"CSRFPreventionToken": csrf})
+        self.session.headers.update({
+            "Authorization": f"PBSAPIToken={token_id}:{token_secret}"
+        })
 
     def get_datastores(self):
         url = f"{self.base_url}/admin/datastore"
         resp = self.session.get(url, verify=False)
         resp.raise_for_status()
-        return resp.json()["data"]
+        return resp.json()
+
+    def get_datastore_status(self, store_name):
+        url = f"{self.base_url}/admin/datastore/{store_name}/status"
+        resp = self.session.get(url, verify=False)
+        resp.raise_for_status()
+        return resp.json()
+
--- sensor.py
+++ sensor.py
@@ -1,36 +1,59 @@
+import logging
 from homeassistant.helpers.entity import Entity
-from .const import DOMAIN
 from .api import ProxmoxBackupAPI
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    host = config.get("host")
-    username = config.get("username")
-    password = config.get("password")
-    
-    api = ProxmoxBackupAPI(host, username, password)
-    datastores = api.get_datastores()
+_LOGGER = logging.getLogger(__name__)
 
-    sensors = [ProxmoxBackupSensor(ds["name"], ds["usage"]) for ds in datastores]
-    add_entities(sensors, True)
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the Proxmox Backup sensors."""
+    host = config.get("pbs_host")
+    token_id = config.get("pbs_token_id")
+    token = config.get("pbs_token")
+
+    try:
+        api = ProxmoxBackupAPI(host, token_id, token)
+        response = api.get_datastores()
+        datastores = response.get("data", [])
+        sensors = []
+
+        for ds in datastores:
+            store_name = ds.get("store")
+            if not store_name:
+                _LOGGER.warning("Datastore missing 'store' key: %s", ds)
+                continue
+
+            usage_response = api.get_datastore_status(store_name)
+            usage = usage_response.get("data", {})
+
+            sensors.append(ProxmoxBackupSensor(store_name, usage))
+
+        add_entities(sensors)
+
+    except Exception as e:
+        _LOGGER.error("Error setting up proxmox_backup sensors: %s", e)
 
 class ProxmoxBackupSensor(Entity):
     def __init__(self, name, usage):
-        self._name = f"PBS Datastore {name}"
-        self._state = usage["used"] / usage["total"] * 100
-        self._attr = {
-            "total": usage["total"],
-            "used": usage["used"],
-            "avail": usage["avail"],
-        }
+        self._name = name
+        self._used = usage.get("used", 0)
+        self._total = usage.get("total", 0)
+        self._avail = usage.get("avail", 0)
 
     @property
     def name(self):
-        return self._name
+        return f"Proxmox Backup {self._name} Usage"
 
     @property
     def state(self):
-        return round(self._state, 2)
+        return self._used
 
     @property
     def extra_state_attributes(self):
-        return self._attr
+        return {
+            "used_bytes": self._used,
+            "total_bytes": self._total,
+            "available_bytes": self._avail,
+            "used_percent": round((self._used / self._total) * 100, 2) if self._total else None,
+            "free_percent": round((self._avail / self._total) * 100, 2) if self._total else None,
+        }
+
